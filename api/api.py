5eb38c52653262c30faa1caf1065a4dde128b3ac
x-esteban <xesteban335@gmail.com>
2021-07-26 17:52:37 +0200

    Improved API response, added random function.

--- api/api.py
+++ api/api.py
@@ -26,7 +26,21 @@ def api_all():
     if 'ccaa' in request.args:
         ccaa_list = eval(request.args['ccaa'])
     weekNumber = str(datetime.date(2021, month, day).isocalendar()[1])
-    response = [df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(1)['town_ID'].tolist()[0], df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(1)['town_ID'].tolist()[0], df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(1)['town_ID'].tolist()[0], df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(1)['town_ID'].tolist()[0], df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(1)['town_ID'].tolist()[0]] 
+    if len(ccaa_list) == 1: 
+        response = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == ccaa_list[0]].sort_values('w'+weekNumber, ascending=False).head(5)['town_ID'].tolist()
+    elif len(ccaa_list) == 15: 
+        response = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']].sort_values('w'+weekNumber, ascending=False).head(5)['town_ID'].tolist()
+    else: 
+        a = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(3)['town_ID'].tolist()[0:3]
+        b = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(3)['town_ID'].tolist()[0:3]
+        c = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(3)['town_ID'].tolist()[0:3]
+        d = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(3)['town_ID'].tolist()[0:3]
+        e = df[['w'+weekNumber, 'town_ID', 'CCAA_ID']][df['CCAA_ID'] == random.choice(ccaa_list)].sort_values('w'+weekNumber, ascending=False).head(3)['town_ID'].tolist()[0:3]
+        response = []
+        for n in [a,b,c,d,e]:
+            for i in n:
+                response.append(i)
+        response = list(set(response))[0:5]
                 
     return jsonify({'destination': response})
 
